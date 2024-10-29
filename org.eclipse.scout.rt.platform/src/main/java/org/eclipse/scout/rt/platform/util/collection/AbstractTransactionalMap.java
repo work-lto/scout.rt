d984263de1d0ee0e104defd00c84b94b2a92457b
Ralph Wortha <steiner.ralph@gmail.com>
2024-10-29 08:33:17 +0100

    AbstractMapTransactionMember#commitChanges must detect commit conflicts
    Before this change, if cache did not contain a value and two
    transactions do at the same time a data change (calls invalidate on
    cache), a commit conflict may have been missed because oldValue in
    removeMap is null.
    
    399399

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/collection/AbstractTransactionalMap.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/collection/AbstractTransactionalMap.java
@@ -1,12 +1,11 @@
 /*
- * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
- * Contributors:
- *     BSI Business Systems Integration AG - initial API and implementation
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.scout.rt.platform.util.collection;
 
@@ -310,16 +309,29 @@ public abstract class AbstractTransactionalMap<K, V> implements Map<K, V> {
         for (Entry<K, V> entry : m_removedMap.entrySet()) {
           K key = entry.getKey();
           V oldValue = entry.getValue();
-          if (oldValue != null) {
-            V insertedValue = m_insertedMap.remove(key);
+          V insertedValue = m_insertedMap.remove(key); // note: this map does not contain null-values
+          if (insertedValue != null) {
             if (!insertsValid) {
               failedCommittedChanges.add(key);
+              continue;
             }
-            else if (insertedValue != null) {
-              if (sharedMap.replace(key, oldValue, insertedValue)) {
-                successfulCommittedChanges.add(key);
-              }
-              else {
+            if (oldValue == null) {
+              // there is a remove and an insert operation - if oldValue of remove operation is null, then we cannot detect commit conflicts
+              failedCommittedChanges.add(key);
+              continue;
+            }
+            boolean replaceValueSuccess = sharedMap.replace(key, oldValue, insertedValue);
+            if (replaceValueSuccess) {
+              successfulCommittedChanges.add(key);
+            }
+            else {
+              failedCommittedChanges.add(key);
+            }
+          }
+          else {
+            if (oldValue == null) {
+              if (sharedMap.containsKey(key)) {
+                // remove entry, and there was no previous value in sharedMap but now there is one
                 failedCommittedChanges.add(key);
               }
             }
@@ -330,13 +342,6 @@ public abstract class AbstractTransactionalMap<K, V> implements Map<K, V> {
               else {
                 failedCommittedChanges.add(key);
               }
-            }
-          }
-          else {
-            // if there must be a value inserted, the loop over insertedMap will handle this
-            if (sharedMap.containsKey(key)) {
-              // remove entry, and there was no previous value in sharedMap but now there is one. Commit failed.
-              failedCommittedChanges.add(key);
             }
           }
         }
