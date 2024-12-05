5ffe8f5a1219f11e25a115e225be781392424e1e
Laura Toedtli <laura.toedtli@bsi-software.com>
2024-12-05 12:22:12 +0100

    PlatformHealthChecker becomes IPlatformListener

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/PlatformHealthChecker.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/PlatformHealthChecker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -10,12 +10,23 @@
 package org.eclipse.scout.rt.server.commons.healthcheck;
 
 import org.eclipse.scout.rt.platform.IPlatform.State;
-import org.eclipse.scout.rt.platform.Platform;
+import org.eclipse.scout.rt.platform.IPlatformListener;
+import org.eclipse.scout.rt.platform.Order;
+import org.eclipse.scout.rt.platform.PlatformEvent;
 
-public class PlatformHealthChecker extends AbstractHealthChecker {
+@Order(PlatformHealthChecker.ORDER) // ensure to be the last platform listener
+public class PlatformHealthChecker extends AbstractHealthChecker implements IPlatformListener {
+  public static final int ORDER = 100_000;
+
+  private volatile boolean m_platformStarted = false;
 
   @Override
   protected boolean execCheckHealth(HealthCheckCategoryId category) throws Exception {
-    return Platform.get().getState() == State.PlatformStarted;
+    return m_platformStarted;
+  }
+
+  @Override
+  public void stateChanged(PlatformEvent event) {
+    m_platformStarted = event.getState() == State.PlatformStarted;
   }
 }
